4310f61c9d9410f6a7460d6f6d36a27482a2ead3
Cabaya <caccabaya@arcnet.com>
2026-03-27 17:52:36 +0800

    Add folder & Class for Repositories in Infrastructure

---
+++ ItemManager.Infrastructure/Repositories/ItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemManager.Core.Interfaces;
+
+namespace ItemManager.Infrastructure.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly string _connectionString;
+
+        public ItemRepository (string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
---
+++ ItemManager.Infrastructure/Repositories/ItemTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemManager.Core.Interfaces;
+
+namespace ItemManager.Infrastructure.Repositories
+{
+    public class ItemTypeRepository : IItemTypeRepository
+    {
+        private readonly string _connectionString;
+        public ItemTypeRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
