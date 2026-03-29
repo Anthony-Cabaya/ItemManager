c96778f4af0ba68fcbca4ba0342120a914d227cc
Anthony.CA <cabaya24.anthony@gmail.com>
2026-03-29 22:00:43 +0800

    Setting up ViewModels

---
+++ ItemManager.Web/ViewModels/ItemTypeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ItemManager.Web.ViewModels
+{
+    public class ItemTypeViewModel
+    {
+        public int ItemTypeID { get; set; } = 0;
+        public string ItemTypeName { get; set; } = string.Empty;
+        public int Sort { get; set; }
+    }
+}
---
+++ ItemManager.Web/ViewModels/ItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ItemManager.Core.Models;
+
+namespace ItemManager.Web.ViewModels
+{
+    public class ItemViewModel
+    {
+        public int ItemID { get; set; } = 0;
+        public string ItemName { get; set; } = string.Empty;
+        public int ItemTypeID { get; set; }
+        public int Sort { get; set; }
+        // Dropdown
+        public List<ItemType> ItemTypes { get; set; } = new List<ItemType>();
+    }
+}
