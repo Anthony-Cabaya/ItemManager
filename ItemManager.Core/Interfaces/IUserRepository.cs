ca80a63852d7b628416fc69c284cca9eac92f137
Cabaya <caccabaya@arcnet.com>
2026-04-01 15:54:53 +0800

    Added Users Models and Interfaces in .Core

---
+++ ItemManager.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ItemManager.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemManager.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByUsernameAsync(string username);
+
+        Task<User?> ValidateUserAsync(string username, string password);
+    }
+}
---
+++ ItemManager.Core/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ItemManager.Core.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        [Required, StringLength(200)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required, StringLength(200)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required, StringLength(200)]
+        public string Role { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; } 
+    }
+}
